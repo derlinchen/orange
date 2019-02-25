4d303df68c120a940328ee67d357e38231dda67a
derlinchen <derlin_nj@163.com>
2019-02-25 11:43:48 +0800

    add page

---
+++ src/main/java/com/orange/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.orange.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+	}
+}
