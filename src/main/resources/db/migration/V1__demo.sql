92c08dcea43373c11823f418a13b943687734cb5
derlinchen <derlin_nj@163.com>
2019-05-25 17:06:20 +0800

    整合flyway

---
+++ src/main/resources/db/migration/V1__demo.sql
@@ -0,0 +1,12 @@
+drop table if exists demo;
+
+/*==============================================================*/
+/* Table: demo                                                  */
+/*==============================================================*/
+
+create table demo
+(
+   id                   varchar(20) not null,
+   name                 varchar(20) not null,
+   primary key (id)
+);
